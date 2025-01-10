63e2561f2305c9f27dfb38a94fe0b2ffccfc712f
Miguel Ángel Ramírez <miguel.ramirez@storicard.com>
2025-01-09 23:52:28 -0600

    Adding empty files for 2024 Updated README with instructions on how to run the code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # advent-of-code
+
+## How to run
+### Input
+```bash
+$ go run main.go -day=01 -part=01 -filePath=day1/data/example.txt
+```
+### Output
+```bash
+2025/01/09 23:47:49 **************************************
+2025/01/09 23:47:49 total: 142
+2025/01/09 23:47:49 elapsed time: 322.792µs
+2025/01/09 23:47:49 **************************************
+````
--- go/2023/main.go
+++ go/2023/main.go
@@ -11,23 +11,29 @@ import (
 )
 
 func main() {
-	day := flag.String("day", "01", "day")
-	part := flag.String("part", "01", "part (01 or 02)")
-	filePath := flag.String("filePath", "", "file path from the example or test data")
+	// variables declaration
+	var (
+		day      string
+		part     string
+		filePath string
+	)
+
+	flag.StringVar(&day, "day", "01", "day")
+	flag.StringVar(&part, "part", "01", "part (01 or 02)")
+	flag.StringVar(&filePath, "filePath", "", "file path from the example or test data")
 
 	flag.Parse()
 
-	if *day == "" || *filePath == "" || *part == "" {
+	if day == "" || filePath == "" || part == "" {
 		log.Panic("either 'day', 'part' or 'filePath' params were not provided")
 	}
 
-	content, err := os.ReadFile(*filePath)
+	content, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Panic("not able to read file:", err.Error())
 	}
 
-	runDay(*day, *part, content)
-
+	runDay(day, part, content)
 }
 
 func runDay(day, part string, content []byte) {
