051268c7cd34fd575a0945fcfff80f6506ff70cd
Miguel Ángel Ramírez <migrmrz@gmail.com>
2025-01-10 00:25:20 -0600

    Adding main file content for 2024 Adding package declaration on empty files Starting with part 1 of day 1

--- go/2024/day1/part1.go
+++ go/2024/day1/part1.go
@@ -1,1 +1,25 @@
 package day1
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+func Part01(input []byte) {
+	// content from file splitted by breaklines
+	splitContent := strings.Split(string(input), "\n")
+
+	var total int // value to be returned as result
+
+	for _, line := range splitContent {
+		if len(line) == 0 {
+			continue
+		}
+
+		fmt.Printf("line: %s, length: %d\n", line, len(line))
+	}
+
+	log.Println("total:", total)
+
+}
--- go/2024/main.go
+++ go/2024/main.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"aoc/day1"
+	"flag"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// variables declaration
+	var (
+		day      string
+		part     string
+		filePath string
+	)
+
+	flag.StringVar(&day, "day", "01", "day")
+	flag.StringVar(&part, "part", "01", "part (01 or 02)")
+	flag.StringVar(&filePath, "filePath", "", "file path from the example or test data")
+
+	flag.Parse()
+
+	if day == "" || filePath == "" || part == "" {
+		log.Panic("either 'day', 'part' or 'filePath' params were not provided")
+	}
+
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Panic("not able to read file:", err.Error())
+	}
+
+	runDay(day, part, content)
+}
+
+func runDay(day, part string, content []byte) {
+	startTime := time.Now()
+
+	log.Println("**************************************")
+
+	switch day {
+	case "01":
+		switch part {
+		case "01":
+			day1.Part01(content)
+			// case "02":
+			// 	day1.Part02(content)
+		}
+		// case "02":
+		// 	switch part {
+		// 	case "01":
+		// 		day2.Part01(content)
+		// 	case "02":
+		// 		day2.Part02(content)
+		// 	}
+		// case "03":
+		// 	switch part {
+		// 	case "01":
+		// 		day3.Part01(content)
+		// 		// case "02":
+		// 		//		day3.Part02(content)
+		// 	}
+	}
+
+	log.Println("elapsed time:", time.Since(startTime))
+
+	log.Println("**************************************")
+}
